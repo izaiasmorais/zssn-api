03b6a65571e8be9f62850a2a2422c189e0f5f731
Izaias Lima <izaiaslima356@gmail.com>
2024-06-15 00:19:01 -0300

    fix: reformulate fastify start

--- src/http/server.ts
+++ src/http/server.ts
@@ -14,7 +14,7 @@ import { registerSurvivor } from "./routes/register-survivor";
 import { editSurvivorLocation } from "./routes/edit-survivor-location";
 import { flagInfected } from "./routes/flag-infected";
 
-const port = process.env.PORT || "3333";
+const port = process.env.PORT || 3333;
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 
@@ -42,6 +42,14 @@ app.register(registerSurvivor);
 app.register(editSurvivorLocation);
 app.register(flagInfected);
 
-app.listen({ port: Number(port) }).then(() => {
-	console.log(`HTTP server running at ${port}`);
-});
+const start = async () => {
+	try {
+		await app.listen({ port: Number(port), host: "0.0.0.0" });
+		app.log.info(`Server listening on port ${port}`);
+	} catch (err) {
+		app.log.error(err);
+		process.exit(1);
+	}
+};
+
+start();
